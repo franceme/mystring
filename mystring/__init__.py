bc4ba81862408e549a36b73df15b79ba33d0bb58
franceme <frantzme@vt.edu>
2023-02-11 22:29:05 -0500

    added stuff delim

--- mystring/__init__.py
+++ mystring/__init__.py
@@ -4,3 +4,11 @@ class string(str):
         return self
     def isempty(self):
         return self.strip() == ''
+    def ad(self, value):
+        self = string(self + getattr(self, 'delim', "")  + value)
+        return self
+    def delim(self, value):
+        self.delim = value
+    def pre(self, value):
+        self = string(value + getattr(self, 'delim', "")  + self)
+        return self
