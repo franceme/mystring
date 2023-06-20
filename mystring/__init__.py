cf49d7dc95ec80eb277391b5478bc925d7ffe486
franceme <frantzme@vt.edu>
2023-06-20 00:29:50 -0400

    Added some nice to haves in the string

--- mystring/__init__.py
+++ mystring/__init__.py
@@ -142,15 +142,15 @@ class string(str):
 					for temp_split_item in temp_item.split(joiner):
 						output_list.append(temp_split_item)
 
-		return output_list
+		return [mystring(x) for x in output_list]
 
 	def tohash(self, hash_type='sha512', encoding='utf-8'):
 		import hashlib
-		return getattr(hashlib, hash_type)(self.encode(encoding)).hexdigest()
+		return mystring(getattr(hashlib, hash_type)(self.encode(encoding)).hexdigest())
 
 	def tobase64(self, encoding='utf-8'):
 		import base64
-		return base64.b64encode(self.encode(encoding)).decode(encoding)
+		return mystring(base64.b64encode(self.encode(encoding)).decode(encoding))
 
 	@staticmethod
 	def frombase64(string, encoding='utf-8'):
