361b07d10de9b0311083e07a7e536bc4dc4e9a48
franceme <frantzme@vt.edu>
2023-07-31 21:03:06 -0400

    another patch

--- mystring/__init__.py
+++ mystring/__init__.py
@@ -306,6 +306,7 @@ try:
 		@staticmethod
 		def from_dbhub_table(table_name, dbhub_apikey, dbhub_owner, dbhub_name):
 			from ephfile import ephfile
+			import pydbhub as dbhub
 			with ephfile("config.ini") as eph:
 				eph += f"""[dbhub]
 	api_key = {dbhub_apikey}
