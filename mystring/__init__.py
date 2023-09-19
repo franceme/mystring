abd45213b58381d2ee58171764848c55b6710f0d
franceme <frantzme@vt.edu>
2023-09-19 15:32:57 -0400

    fix wrap

--- mystring/__init__.py
+++ mystring/__init__.py
@@ -162,7 +162,7 @@ class string(str):
 	@staticmethod
 	def frombase64(string, encoding='utf-8'):
 		import base64
-		return base64.b64decode(string.encode(encoding)).decode(encoding)
+		return string(base64.b64decode(string.encode(encoding)).decode(encoding))
 
 	@staticmethod
 	def ofrom(strung):
