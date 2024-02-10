5616afca4e03c800697f6655a956e181ea6affdc
franceme <frantzme@vt.edu>
2024-02-10 02:45:56 +0000

    repatch

--- mystring/__init__.py
+++ mystring/__init__.py
@@ -28,9 +28,9 @@ def levenshtein_distance(first, second, percent=True):
 	second_length = len(second) + 1
 	distance_matrix = [[0] * second_length for x in range(first_length)]
 	for i in range(first_length):
-	   distance_matrix[i][0] = i
+		distance_matrix[i][0] = i
 	for j in range(second_length):
-	   distance_matrix[0][j]=j
+		distance_matrix[0][j]=j
 	for i in range(1, first_length):
 		for j in range(1, second_length):
 			deletion = distance_matrix[i-1][j] + 1
