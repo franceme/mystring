02ff3697ab92fabbf8bee034d5eea780fc233689
franceme <frantzme@vt.edu>
2024-03-16 17:30:34 -0400

    rr

--- mystring/__init__.py
+++ mystring/__init__.py
@@ -1105,7 +1105,7 @@ try:
 		def __delitem__(self, key):del self.dyct[key]
 		def __iter__(self):return iter(self.dyct.values())
 		def __reversed__(self):return reversed(self.dyct.values())
-		def __contains__(self, item):return item in self.dyct
+		def __contains__(self, item):return item in self.dyct.keys()
 		def __getattr__(self, name):return self[name]
 		def __setattr__(self, name, value):self[name] = value
 		def __delattr__(self, name): del self[name]
